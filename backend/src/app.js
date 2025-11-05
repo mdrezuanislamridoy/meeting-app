b4bf94c41b2fd1bd1e70ad7b78d05ff8146773d0
mdrezuanislamridoy <mdrezuanislamridoy@gmail.com>
2025-11-05 19:57:21 +0600

    made a single link for server and client

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,20 @@
 import express from "express";
+import path from "path";
+import env from "./config/dotenv.js";
 
 const app = express();
+
+const __dirname = path.resolve();
 
 // middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+if (env.node_env === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+  });
+}
 
 export default app;
--- backend/src/config/dotenv.js
+++ backend/src/config/dotenv.js
@@ -4,6 +4,7 @@ configDotenv();
 const env = {
   port: process.env.PORT,
   mongoUrl: process.env.MONGO_URL,
+  node_env: process.env.NODE_ENV,
 };
 
 export default env;

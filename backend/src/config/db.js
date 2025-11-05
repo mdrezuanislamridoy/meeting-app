058d9fd20cf6e461b215e89d615aba0c8269e517
mdrezuanislamridoy <mdrezuanislamridoy@gmail.com>
2025-11-05 20:29:53 +0600

    redeploy

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,8 +1,12 @@
 import mongoose from "mongoose";
 import env from "./dotenv.js";
 const db = async () => {
-  await mongoose.connect(env.mongoUrl);
-  console.log("Db Connected");
+  try {
+    await mongoose.connect(env.mongoUrl);
+    console.log("Db Connected");
+  } catch (error) {
+    console.log("Db conection failed");
+  }
 };
 
 export default db;
